73ff9da29da8000710866dab9e52e378099fc968
Shaun Norton <shaunnorton87@gmail.com>
2022-03-23 13:11:27 -0700

    Added base html file and created html for wiki page

--- src/main/routes.py
+++ src/main/routes.py
@@ -5,3 +5,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def MainPage():
     return render_template("home.html")
+
+@main.route("/wiki")
+def WikiPage():
+    return render_template("wiki.html")
