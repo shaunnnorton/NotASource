bfe1f3d19083bdbc094c5da026be731307cec086
Shaun Norton <shaunnorton87@gmail.com>
2023-02-25 14:38:15 -0800

    nothing

--- src/utils/process.py
+++ src/utils/process.py
@@ -3,3 +3,12 @@ from bs4 import BeautifulSoup
 from requests import Response
 
 #This file contains functions to process data recieved from other functions or external sources. 
+def format_refrences(soup: BeautifulSoup):
+    ref = {
+        "text":'',
+        "links":'',
+        "ref_num":0
+    }
+    reflist = []
+    for data in soup:
+        ref.links = data.find_all("a")
